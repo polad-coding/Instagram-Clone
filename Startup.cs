a0f6a0d9ea5dcf25b0ce9aa797a581ad28816dbc
polad-coding <62692608+polad-coding@users.noreply.github.com>
2020-05-27 10:43:46 +0300

    Added forwarded headers

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace InstagramClone
 {
@@ -55,6 +56,11 @@ namespace InstagramClone
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
+            app.UseForwardedHeaders(new ForwardedHeadersOptions {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto 
+            });
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
